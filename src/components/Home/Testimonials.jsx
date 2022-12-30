bc5d2ba40d7407d5185b8af7e1f7fe80853e9a44
Anshika <anshika2011087@akgec.ac.in>
2022-12-30 20:15:17 +0530

    testimonial

--- src/components/Home/Testimonials.jsx
+++ src/components/Home/Testimonials.jsx
@@ -8,41 +8,29 @@ const Testimonials = () => {
     <div className='container'>
     <section>
   <h1>Our Services</h1>
-  <p className="section-lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+  <p className="section-lead">The guaranteed formula to be a Study wizard</p>
   <div className="services-grid">
     <div className="service service1">
-    <svg xmlns="http://www.w3.org/2000/svg" className="features-icon" fill="none" viewBox="0 0 24 24"
-            stroke="currentColor">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-              d="M19.428 15.428a2 2 0 00-1.022-.547l-2.387-.477a6 6 0 00-3.86.517l-.318.158a6 6 0 01-3.86.517L6.05 15.21a2 2 0 00-1.806.547M8 4h8l-1 1v5.172a2 2 0 00.586 1.414l5 5c1.26 1.26.367 3.414-1.415 3.414H4.828c-1.782 0-2.674-2.154-1.414-3.414l5-5A2 2 0 009 10.172V5L8 4z" />
-          </svg>
+    <img src="images/test1.svg" alt="hero image" className="hero-img" />
       <i className="ti-bar-chart"></i>
-      <h4>Wealth Management</h4>
-      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      <h4>Adaptive learning</h4>
+      <p>Children can choose a topic and work at their own pace</p>
       {/* <a href="#" className="cta">Read More <span className="ti-angle-right"/></a> */}
     </div>
 
     <div className="service service2">
-    <svg xmlns="http://www.w3.org/2000/svg" className="features-icon" fill="none" viewBox="0 0 24 24"
-            stroke="currentColor">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-              d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-          </svg>
+    <img src="images/test2.svg" alt="hero image" className="hero-img" />
       <i className="ti-light-bulb"></i>
-      <h4>Financial Planning</h4>
-      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      <h4>Interactive classes</h4>
+      <p>A combination of group and individualised exercises</p>
       {/* <a href="#" className="cta">Read More <span className="ti-angle-right"/></a> */}
     </div>
 
     <div className="service service3">
-    <svg xmlns="http://www.w3.org/2000/svg" className="features-icon" fill="none" viewBox="0 0 24 24"
-            stroke="currentColor">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-              d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
-          </svg>
+           <img src="images/test3.svg" alt="hero image" className="hero-img" />
       <i className="ti-money"></i>
-      <h4>Investment Banking</h4>
-      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      <h4>Expert teachers</h4>
+      <p>A personal tutor with you, every step of the way</p>
       {/* <a href="#" className="cta">Read more <span className="ti-angle-right"></span></a> */}
     </div>
   </div>
@@ -65,12 +53,20 @@ const Wrapper = styled.section`
   box-sizing: inherit;
 }
 
-.features-icon {
-  stroke:#425fca;                /*???*/
-  width: 4rem;
-  height: 4rem;
-  margin-bottom: 1rem;
-  margin-top:1rem;
+// .features-icon {
+//   stroke:#425fca;                /*???*/
+//   width: 4rem;
+//   height: 4rem;
+//   margin-bottom: 1rem;
+//   margin-top:1rem;
+// }
+img{
+  object-fit: contain;
+  width: 63px;
+  height: 63px;
+  margin:0 78px
+
+
 }
 
 html {
