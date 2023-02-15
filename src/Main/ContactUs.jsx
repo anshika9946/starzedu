83b5a28d06afcb8f7796a36c7c5dba49257d69c3
Anshika <anshika2011087@akgec.ac.in>
2023-02-15 22:31:55 +0530

    ..

--- src/Main/ContactUs.jsx
+++ src/Main/ContactUs.jsx
@@ -6,7 +6,7 @@ import WeOffer from "../components/WeOffer";
 import Contact from "../components/Contact";
 import Colorful from "../components/Colorful";
 import JustContact from "./JustContact";
-import Formm from "../components/Formm";
+// import Formm from "../components/Formm";
 
 //import { useGlobalContext } from "./context";
 
@@ -14,7 +14,7 @@ const ContactUs = () => {
 
   return (
     <>
-    <Formm/>
+    {/* <Formm/> */}
 <Design/>
 <Testimonials/>
 
