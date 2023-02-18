c67d3a1b59edc9a15e4e0481b71e044195a6eb41
Anshika <anshika2011087@akgec.ac.in>
2023-02-18 15:28:39 +0530

    contact us

--- src/Main/ContactUs.jsx
+++ src/Main/ContactUs.jsx
@@ -7,7 +7,7 @@ import Contact from "../components/Contact";
 import Colorful from "../components/Colorful";
 import JustContact from "./JustContact";
 import Formm from "../components/Formm";
-import FormP from "../components/FormP";
+
 
 
 
@@ -18,7 +18,7 @@ const ContactUs = () => {
   return (
     <>
    
-   <FormP/>
+
     <Formm/>
 {/* <Design/> */}
 <Testimonials/>
