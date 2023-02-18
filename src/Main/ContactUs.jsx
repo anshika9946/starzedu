a9de422d1bbb1f4e250c0a6c4f2a5a45ede93b8e
Anshika <anshika2011087@akgec.ac.in>
2023-02-18 10:12:24 +0530

    demo form

--- src/Main/ContactUs.jsx
+++ src/Main/ContactUs.jsx
@@ -1,12 +1,14 @@
 import React, { useEffect } from "react";
-import Design from "../components/Design";
+// import Design from "../components/Design";
 import HeroSection from "../components/HeroSection";
 import Testimonials from "../components/Testimonials";
 import WeOffer from "../components/WeOffer";
 import Contact from "../components/Contact";
 import Colorful from "../components/Colorful";
 import JustContact from "./JustContact";
-// import Formm from "../components/Formm";
+import Formm from "../components/Formm";
+import Popp from "./Popp";
+
 
 //import { useGlobalContext } from "./context";
 
@@ -14,8 +16,10 @@ const ContactUs = () => {
 
   return (
     <>
-    {/* <Formm/> */}
-<Design/>
+    <Popp/>
+   
+    <Formm/>
+{/* <Design/> */}
 <Testimonials/>
 
 
