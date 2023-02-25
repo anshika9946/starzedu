00af3700b9e39b3b004983f1f57dd6e0f3217739
Anshika <anshika2011087@akgec.ac.in>
2023-02-25 17:42:18 +0530

    courses

--- src/Main/Courses.jsx
+++ src/Main/Courses.jsx
@@ -9,15 +9,21 @@ import Timeline from "../components/Timeline";
 //import { useGlobalContext } from "./context";
 
 const Courses = () => {
+  const tabs = [
+    
+    { label: 'Australia', content: 'Provides primary, secondary and high secondary education. Primary is from kindergarten to year 6, secondary school from grade 7-10, and senior-secondary or high school is from grade 11-12. Syllabus applicable- Australian Curriculum NSW/Victorian/Queensland Competitive exams Selective, NAPLAN, HAST, SAC, ICAS' },
+    { label: 'Canada', content: 'Comprises of 10 provinces, and three territories. Education over there is a province-based system of public education provided, funded and administered by federal, provincial, and local governments. Elementary Schools – Grade 1-6 Secondary Schools: Junior High Schools – Grade 7-9 High School – Grades 10-12 ' },
+    { label: 'USA', content: 'The system is broken down into three stages:Elementary school (Grades K–5) Middle school (Grades 6–8)High school (Grades 9–12At the end of high school, pupils are also provided with a Grade Point Average (GPA), which can help to determine their next step either into work or college' },
 
+  ];
   return (
     <>
-<Course/>
-      
       {/* <WeOffer/> */}
+      
       {/* <GetStarted/> */}
      
-      {/* <Timeline/> */}
+      <Timeline tabs={tabs} defaultActiveTab={1}/>
+      <Course/>
 
     </>
   );
--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,191 +1,95 @@
-import React from 'react'
+import { useState } from 'react';
 import styled from "styled-components";
 
 
-const Timeline = () => {
-	
+function Timeline(props) {
+  const [activeTab, setActiveTab] = useState(props.defaultActiveTab || 0);
+
+  const handleTabClick = (index) => {
+    setActiveTab(index);
+  };
+
   return (
     <Wrapper className="section">
-    
-  
-    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css"/>
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-
-
-
-<div className="">
-  <div className="container">
-    <h2 className="pb-3 pt-2 border-bottom mb-5">OUR PROGRAM</h2>
-    
-    <div className="row align-items-center how-it-works d-flex">
-      <div className="col-2 text-center bottom d-inline-flex justify-content-center align-items-center">
-        <div className="circle font-weight-bold">1</div>
-      </div>
-      <div className="col-6">
+      <div className="tab-menu">
         
-        <p id="milestone_1_description">Dedicated one hour session with the tutor</p>
+        {props.tabs.map((tab, index) => (
+          <div
+            key={index}
+            className={`tab-menu-item ${index === activeTab ? 'active' : ''}`}
+            onClick={() => handleTabClick(index)}
+          >
+            {tab.label}
+          </div>
+        ))}
       </div>
-    </div>
-  
-    <div className="row timeline">
-      <div className="col-2">
-        <div className="corner top-right"></div>
+      <div className="tab-content">
+        {props.tabs.map((tab, index) => (
+          <div
+            key={index}
+            className={`tab-content-item ${index === activeTab ? 'active' : ''}`}
+          >
+            {tab.content}
+          </div>
+        ))}
       </div>
-      <div className="col-8">
-        <hr/>
-      </div>
-      <div className="col-2">
-        <div className="corner left-bottom"></div>
-      </div>
-    </div>
-   
-    <div className="row align-items-center justify-content-end how-it-works d-flex">
-      <div className="col-6 text-right">
-        
-        <p id="milestone_2_description">Flexible Class Timings</p>
-      </div>
-      <div className="col-2 text-center full d-inline-flex justify-content-center align-items-center">
-        <div className="circle font-weight-bold">2</div>
-      </div>
-    </div>
- 
-    <div className="row timeline">
-      <div className="col-2">
-        <div className="corner right-bottom"></div>
-      </div>
-      <div className="col-8">
-        <hr/>
-      </div>
-      <div className="col-2">
-        <div className="corner top-left"></div>
-      </div>
-    </div>
-  
-    <div className="row align-items-center how-it-works d-flex">
-      <div className="col-2 text-center more d-inline-flex justify-content-center align-items-center">
-        <div className="circle font-weight-bold">3</div>
-      </div>
-      <div className="col-6">
-        
-        <p id="milestone_3_description">Highly-customized Lesson Plans</p>
-      </div>
-    </div>
-		 <div className="row timeline">
-      <div className="col-2">
-        <div className="corner top-right"></div>
-      </div>
-      <div className="col-8">
-        <hr/>
-      </div>
-      <div className="col-2">
-        <div className="corner left-bottom"></div>
-      </div>
-    </div>
-  
-	<div className="row align-items-center justify-content-end how-it-works d-flex">
-      <div className="col-6 text-right">
-        
-        <p id="milestone_4_description">Flexibility for Parent-Teacher Interaction Anytime During The Class</p>
-      </div>
-      <div className="col-2 text-center morethan d-inline-flex justify-content-center align-items-center">
-        <div className="circle font-weight-bold">4</div>
-      </div>
-    </div>
-</div>
-	</div>
     </Wrapper>
     );
 };
 
 const Wrapper = styled.section`
 font-family:Microsoft Sans Serif;
-margin-bottom:4rem;
-.circle {
-  font-weight:bold;
-  font-size:6rem;
-  colour:white;
+    padding: 9rem 0;
+    box-sizing: border-box;
+    justify-content: center;
+    margin-top:0;
+
+.tab-menu {
+  display: flex;
+  justify-content: center;
+
+  font-size:2rem;
+  
 }
-.circle {
-    padding: 60px 60px;
-    border-radius: 50%;
-    background-color: var(--primary);
-    color: #ffbf00;
-    max-height: 50px;
-    z-index: 2;
-  }
-  
-  .how-it-works.row .col-2 {
-    align-self: stretch;
-  }
-  .how-it-works.row .col-2::after {
-    content: "";
-    position: absolute;
-    border-left: 3px solid var(--primary);
-    z-index: 1;
-  }
-  .how-it-works.row .col-2.bottom::after {
-    height: 50%;
-    left: 50%;
-    top: 50%;
-  }
-  .how-it-works.row .col-2.full::after {
-    height: 100%;
-    left: calc(50% - 3px);
-  }
-  .how-it-works.row .col-2.more::after {
-    height: 100%;
-    left: calc(50% - 0px);
-  }
-  .how-it-works.row .col-2.top::after {
-    height: 50%;
-    left: 50%;
-    top: 0;
-  }
-  .how-it-works.row .col-2.morethan::after {
-    height: 50%;
-    left: calc(50% - 3px);
-    top: 0;
-  }
-  
-  
-  .timeline div {
-    padding: 0;
-    height: 40px;
-  }
-  .timeline hr {
-    border-top: 3px solid var(--primary);
-    margin: 0;
-    top: 17px;
-    position: relative;
-  }
-  .timeline .col-2 {
-    display: flex;
-    overflow: hidden;
-  }
-  .timeline .corner {
-    border: 3px solid var(--primary);
-    width: 100%;
-    position: relative;
-    border-radius: 15px;
-  }
-  .timeline .top-right {
-    left: 50%;
-    top: -50%;
-  }
-  .timeline .left-bottom {
-    left: -50%;
-    top: calc(50% - 3px);
-  }
-  .timeline .top-left {
-    left: -50%;
-    top: -50%;
-  }
-  .timeline .right-bottom {
-    left: 50%;
-    top: calc(50% - 3px);
-  }
-  
+
+.tab-menu-item {
+  padding: 10px;
+  cursor: pointer;
+  font-weight: bold;
+  color: #555;
+}
+
+.tab-menu-item:hover {
+  background-color: #eee;
+}
+
+.tab-menu-item.active {
+  border-bottom: 3px solid #0099ff;
+  color: #0099ff;
+}
+
+.tab-content {
+  margin-top:2rem;
+
+  border: 2px solid black;
+  padding: 4rem;
+  font-size:2rem;
+  justify-content: center;
+  display: flex;
+  margin: auto;
+  width: 80%; 
+}
+
+
+.tab-content-item {
+  display: none;
+  padding: 2rem;
+}
+
+.tab-content-item.active {
+  display: block;
+}
+
 `;
 
 export default Timeline ;
